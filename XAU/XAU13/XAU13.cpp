aaf062e41e5c47fc38ec8cd434a61a41428c076b
Hoàng Anh <107913966+Hanhdeptrai11@users.noreply.github.com>
2022-09-12 22:31:38 +0700

    Create XAU13.cpp

---
+++ XAU/XAU13/XAU13.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int numberMax(int n)
+{
+    if (n == 0) return 0;
+    n = abs(n);
+    int s = 0;
+    while (n > 0)
+        {
+        int k = n % 10;
+        n /= 10;
+        if (k > s)
+        s = k;
+    }
+    return s;
+}
+int main()
+{
+    int x;
+    cin >> x;
+    cout << numberMax(x);
+    return 0;
+}
